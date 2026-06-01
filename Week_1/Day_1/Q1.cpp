775b365122c75a8a61f1f6a558aa3bc972a36205
heyprashantsingh <25116cn077@glbitm.ac.in>
2026-06-01 16:10:28 +0530

    day 1 question added

--- Week_1/Day_1/Q1.cpp
+++ Week_1/Day_1/Q1.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 int main(){
     int i = 1 , sum = 0 , n ;
-    cout << (" enter the number : "); // giving output
-    cin >> n ; // taking input form user
-     for ( i = 1 ; i <=n ; i++){ // using for loop
+    cout << (" enter the number : "); 
+    cin >> n ; 
+     for ( i = 1 ; i <=n ; i++){ 
         sum = sum + i ;
      }
      cout << (" sum of first n natural number : ") << sum << endl;
