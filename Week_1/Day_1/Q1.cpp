8ef12bf4a5c4b3c05a32016fd831f99da819b560
PRASHANT SINGH <singhhouse843@gmail.com>
2026-06-01 19:06:26 +0530

    Day 1 question added

--- Week_1/Day_1/Q1.cpp
+++ Week_1/Day_1/Q1.cpp
@@ -5,8 +5,8 @@ int main(){
     int i = 1 , sum = 0 , n ;
     cout << (" enter the number : "); 
     cin >> n ; 
-     for ( i = 1 ; i <=n ; i++){ 
-      sum = sum + i ;
+    for ( i = 1 ; i <=n ; i++){ 
+    sum = sum + i ;
      }
      cout << (" sum of first n natural number : ") << sum << endl;
      return 0;
