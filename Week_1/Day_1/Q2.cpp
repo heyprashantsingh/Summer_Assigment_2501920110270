49d8ded606903f6b330296915fbcdd1aeb8fdc19
PRASHANT SINGH <singhhouse843@gmail.com>
2026-06-01 19:07:37 +0530

    Day 1 question added

--- Week_1/Day_1/Q2.cpp
+++ Week_1/Day_1/Q2.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 int main () {
     int i = 1 , n;
-      cout << (" enter the number : "); // giving output
-      cin >> n ; // taking input
+      cout << (" enter the number : "); 
+      cin >> n ; 
        
       for ( i = 1 ; i <=10 ; i++) {
        cout << n << " * " << i << " = " << n * i << endl;
