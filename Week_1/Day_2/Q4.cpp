941dd98f41f8d1ce2c503ddd46293b60ab68e66d
PRASHANT SINGH <singhhouse843@gmail.com>
2026-06-02 17:12:53 +0530

    all question added

--- Week_1/Day_2/Q4.cpp
+++ Week_1/Day_2/Q4.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 int main (){
    int n, reverse = 0 ;
-  int  digit = n;
+  
   cout << (" enter the number : ");
   cin >> n;
+  int  x = n;
      while (n>0){
-      digit = n % 10 ;                                       
+     int  digit = n % 10 ;  
+      reverse = reverse * 10 + digit;                                     
         n = n/10;
-    reverse = reverse * 10 + digit;
  }
-     if ( digit == reverse){
+     if ( x == reverse){
       cout << " the number is palindrome" << endl;
      } else {
       cout << " the number is not palindrome" << endl;
