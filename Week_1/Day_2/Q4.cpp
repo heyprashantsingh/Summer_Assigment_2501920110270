f510482e1526621bbe607384798c84827e4a7ca8
PRASHANT SINGH <singhhouse843@gmail.com>
2026-06-02 17:01:18 +0530

    Add files via upload

---
+++ Week_1/Day_2/Q4.cpp
@@ -0,0 +1,20 @@
+// write a program to check whether the number is palindrome
+#include <iostream>
+using namespace std;
+int main (){
+   int n, reverse = 0 ;
+  int  digit = n;
+  cout << (" enter the number : ");
+  cin >> n;
+     while (n>0){
+      digit = n % 10 ;                                       
+        n = n/10;
+    reverse = reverse * 10 + digit;
+ }
+     if ( digit == reverse){
+      cout << " the number is palindrome" << endl;
+     } else {
+      cout << " the number is not palindrome" << endl;
+     }
+   return 0;
+}
