61f436959b17bb7a4c73e3e24912b7377f6331de
PRASHANT SINGH <singhhouse843@gmail.com>
2026-06-04 11:31:27 +0530

    Add files via upload

--- Week_1/Day_3/Q1.cpp
+++ Week_1/Day_3/Q1.cpp
@@ -1,1 +1,26 @@
+// WWRITE A PROGRAM TO CHECK WHETHER A NUMBER IS PRIME
+#include <iostream>
+using namespace std;
+int main (){
+    int n, count = 0 ;
+    cout << ("enter the number :");
+    cin >> n ;
+
+    for ( int i =1 ; i<= n ; i++) {
+
+        if  (n % i == 0) { 
+                count++  ;
+           }
+    }
+        if (count == 2)
+        {
+          cout<< ("it is a prime number") << endl; 
+        }  
+        else{
+            cout << (" it is not a prime number ") << endl;
+        }
+        return 0;
+    }
+
+
 
