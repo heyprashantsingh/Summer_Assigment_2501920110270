7b2e8dba766550350c86d06d47d363f5390dea39
PRASHANT SINGH <singhhouse843@gmail.com>
2026-06-06 13:47:20 +0530

    Q1.cpp
    Implement Fibonacci series generation based on user input.

--- Week_1/Day_4/Q1.cpp
+++ Week_1/Day_4/Q1.cpp
@@ -1,1 +1,18 @@
+// write a program to generate fibonacci series
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n, a = 0, b = 1, c;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        cout << a << " ";
+         c = a + b;
+         a = b;
+         b = c;
+    }
+
+    return 0;
+}
